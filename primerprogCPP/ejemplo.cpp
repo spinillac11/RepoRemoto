ebb116eb4f2f7e045bbb2c52f2f4a7ddabad555e
Santiago Pinilla <santiago.inilla@gmail.com>
2021-10-14 23:52:51 -0500

    Primer ejemplo

---
+++ primerprogCPP/ejemplo.cpp
@@ -0,0 +1,19 @@
+//Calcular los numeros primos entre 1 y 100
+// Al inicio del programa se ponen las librerías a usar
+
+#include <iostream>  //Leer entradas estandar e imprimir cosas
+#include <cmath>
+
+
+//main function:
+
+int main(void) //int indica a la funcion main a retornar un entero, void no atribuye ningun valor
+{
+
+  int m = 1;
+  int n = 100; //creacion de variable
+  std::cout << "m: " << m << "\n";
+  std::cout << "n: " << n << "\n";
+  
+  return 0; //Es necesario un retorno siempre, el ; permite identificar el final de un argumento
+}
