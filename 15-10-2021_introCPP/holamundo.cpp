e820c6b2c3e8e63d01d4b0b74a4c6c4ca4622c4c
Santiago Pinilla <santiago.inilla@gmail.com>
2021-10-15 09:08:11 -0500

    Ejemplos de variables y hola mundo

---
+++ 15-10-2021_introCPP/holamundo.cpp
@@ -0,0 +1,25 @@
+// Edicion: escribir mi programa con un editor
+// Compilacion: g++ holamundo.cpp
+// Ejecucion: ./a.out
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "hola mundo\n"; //cout nos permite comunicarnos con la salida estandar
+
+  int a; // idealmente tengo que otorgarle un valor inicial a la variable
+  float x = 0.0;
+  double y = 0.0;
+  std::cout <<"a= " << a << "\n"; // Concateno texto
+  std::cout <<"x= " << x << "\n";
+  std::cout <<"y= " << y << "\n"; 
+
+  return 0; 
+}
+
+// Variables: sting, bool, int, float, double, char
+// Operadores: +, -, *, / 
+// funciones
+// estructuras de datos basicas
+// clases
