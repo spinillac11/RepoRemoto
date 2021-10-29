e43cca48297471bc1a808fbb2347407a6b743cca
Santiago Pinilla <santiago.inilla@gmail.com>
2021-10-29 07:36:25 -0500

    added a little optimization to original

--- 27-10-2021_funciones1/euler3-basic.cpp
+++ 27-10-2021_funciones1/euler3-basic.cpp
@@ -14,7 +14,7 @@ bool isprime (long long n); // declaracion, funcion que retorna un bool
 
 int main(void)
 {
-    const long long N = 600851475143; //No hay que darle tantas libertades al codigo de modificar variables
+    const long long N = 600851475147; //No hay que darle tantas libertades al codigo de modificar variables
     std::cout << largestprimediv(N) << std::endl;
     
     return 0;
@@ -23,26 +23,28 @@ int main(void)
 //Implemento despues de la funcion main para facilidad de lectura y no llenar el inicio de funciones
 long long largestprimediv (long long n) //No importa que el parametro se repita por que son variables locales
 {
+    long long result = 0;
+
     for(long long ii = 2; ii < n; ii++){ //Los divisores vienen de a pares por lo que solo necesito verificar hasta el sqrt(N) 
         if(n%ii == 0){
             //Divisores
             //std::cout << ii << std::endl;
             long long largedivisor = n/ii;
             if(isprime(largedivisor) == true){ //Llamo a la funcion e introduzco largedivisor como parametro imicial
-            std::cout << largedivisor << std::endl;
+            result = largedivisor;
             break;
             } 
 
         }
     }
-    return 0;
+    return result;
 }
 
 bool isprime (long long n) // Funcion: Defino los parametros que 
 {
     bool isprime = true; //Supongo que es primo
 
-            for(long long jj = 2; jj < n; jj++){
+            for(long long jj = 2; jj <= std::sqrt(n); jj++){ //Como los divisores van de a apares al poner la raiz optimizo el codigo
                 if(n%jj == 0){ 
                     isprime = false; //Si no cumple la condicion cambio la bandera
                     break;
