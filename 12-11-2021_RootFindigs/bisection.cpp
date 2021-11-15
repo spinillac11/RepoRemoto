45c53cfc624c3da4f97e2e325f1d5abf4620f111
Santiago Pinilla <santiago.inilla@gmail.com>
2021-11-15 14:39:48 -0500

    Added reference call to niter counter

--- 12-11-2021_RootFindigs/bisection.cpp
+++ 12-11-2021_RootFindigs/bisection.cpp
@@ -4,7 +4,8 @@
 
 double f(double x);
 template <class fptr>
-double bisection(double xl, double xu, fptr fun, double eps);
+double bisection(double xl, double xu, fptr fun, double eps, int & niter); //SI quiero que una variable que llamo dentro de la funcion cambie por fuera de esta
+// Entonces no solo tengo que asignarle un valor sino en la funcion sino llamarla con su direccion de memoria, para esto se usa el &, para llamar a una variable por referencia 
 
 int main(int argc, char **argv)
 {
@@ -13,8 +14,9 @@ int main(int argc, char **argv)
   const double XL = std::atof(argv[1]);
   const double XU = std::atof(argv[2]);
   const double EPS = std::atof(argv[3]);
-
-  std::cout << bisection(XL, XU, f, EPS) << std::endl;
+  int counter = 0;
+  std::cout << bisection(XL, XU, f, EPS, counter) << std::endl;
+  std::cout << counter << std::endl;
   
   return 0;
 }
@@ -25,19 +27,22 @@ double f(double x)
 }
 
 template <class fptr>
-double bisection(double xl, double xu, fptr fun, double eps)
+double bisection(double xl, double xu, fptr fun, double eps, int & niter)
 {
   double xr = 0;
+  int iter = 1; //COntador para guardar el numero de iteraciones
   for( ;  ; ) { // while(true)
     xr = (xl + xu)/2; // bisection
     // encontre la raiz?
     if (std::fabs(fun(xr)) <= eps) break;
     // actualizar los limites
+    iter++;
     if (fun(xr)*fun(xl) < 0) {
       xu = xr;
     } else {
       xl = xr;
     }
   }
+  niter = iter; //Le asignamos el valor a la variable que llamamos de niter
   return xr;
 }
