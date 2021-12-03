036e5aad8c101429411c11adf61ce8cb8bb859dd
Santiago Pinilla <live@slackware.example.org>
2021-12-02 23:45:55 -0500

    Sobrecarga del operador asterisco corregida

--- 2021-12-01_Tarea/VectorClass.cpp
+++ 2021-12-01_Tarea/VectorClass.cpp
@@ -1,39 +1,33 @@
 #include<iostream>
-
 struct Vector3D
 {
     double x, y, z;
-
+    //Constructor
     Vector3D(){
-         x = 0.0;
-         y = 0.0;
-         z = 0.0;
+        x = 0.0; y = 0.0; z = 0.0;
     }
-
+    //Print vector
     void print(){
         std::cout << "(" << x << ", " << y << ", " << z << ")" << "\n";
     }
-
+    //Overloading operator +
     Vector3D operator+(const Vector3D &v){
         Vector3D sum;
-            sum.x = x + v.x;
-            sum.y = y + v.y;
-            sum.z = z + v.z;
-            return sum;
+        sum.x = x + v.x;
+        sum.y = y + v.y;
+        sum.z = z + v.z;
+        return sum;
     }
-
-    Vector3D operator*(const Vector3D &v){
-        Vector3D dot;
-        dot.x = x*v.x;
-        dot.y = y*v.y;
-        dot.z = z*v.z;
-        return dot;
+    //Overloading operator *
+    double operator*(const Vector3D &v){
+        return (x*v.x) + (y*v.y) + (z*v.z);
     }
-
+    ~Vector3D(){} //Destructor
 };
 
 int main()
 {
+    double dotp = 0.0;
     Vector3D R1, R2, R3;
     R1.x = 1.0; R1.y = 2.0; R1.z = 3.0; 
     R2.x = 4.0; R2.y = 5.0; R2.z = 6.0; 
@@ -41,8 +35,8 @@ int main()
     R3 = R1 + R2;
     R1.print(); R2.print(); R3.print();
 
-    R3 = R1*R2;
-    std::cout << R3.x + R3.y + R3.z << std::endl;
+    dotp = R1*R2;
+    std::cout << dotp << std::endl;
 
     return 0;
 }
