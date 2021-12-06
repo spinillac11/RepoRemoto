61b3910dde5bf7f51f91b17c0118326443c4029e
Santiago Pinilla <live@slackware.example.org>
2021-12-06 17:05:20 -0500

    Simulacion de una pelota rebotado

---
+++ 2021-12-03-OOP-MD-II/md-simul.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include "particle.h"
+#include "integrator.h"
+
+// This could on itws own class: Config
+const double G = -9.81;
+const int NSTEPS = 1000;
+const double DT = 0.01;
+const double K = 253.57; // we will model the floor as a spring
+const double LR = 0.9; 
+const double LL = -0.25; 
+
+
+// function declarations
+void initial_conditions(Particle & body);
+void compute_force(Particle & body);
+void print(Particle & body, double time);
